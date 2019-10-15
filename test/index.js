6f6168ae3cf6e44dcc539a7307e005601e74246e
shoestrong <shoestrong.zhang@ly.com>
2019-10-15 16:49:51 +0800

    :wrench: fix file location change

--- test/index.js
+++ test/index.js
@@ -1,14 +1,7 @@
-/*
- * @Author: shoestrong
- * @Date: 2019-09-23 12:00:33
- * @Description: file content
- * @LastEditTime: 2019-09-23 16:31:32
- * @LastEditors: shoestrong
- */
-const BuildI18nLangConfig = require('../index')
+const BuildI18nLangConfig = require('../lib/index')
 
 new BuildI18nLangConfig({
   defaultLangs: ['zh_CN', 'en'],
   entryPath: './local',
-  outPath: './user/langs'
+  outPath: './langs/user'
 })
